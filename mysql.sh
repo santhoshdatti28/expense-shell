eae56df39ef067ff97f9162b670c70b8f6e04e02
ManikantaDatti <manikantadatti11@gmail.com>
2025-02-06 12:35:50 +0530

    mysql.sh file commiting

--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 userid=$(id -u)
+
 logs_folder="/var/log/expenseshell-log"
 log_file=$(echo $0 | cut -d "." -f1)
 timestamp=$(date +%Y-%m-%d-%H-%M-%S)
 log_file_name="$logs_folder/$log_file-$timestamp.log"
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -18,6 +22,7 @@ VALIDATE(){
     fi
 
 }
+
 CHECK_ROOT(){
     if [ $userid -ne 0 ]
     then 
@@ -25,15 +30,21 @@ CHECK_ROOT(){
         exit1
     fi
 }
+
 mkdir -p $logs_folder
 
 echo "script started and executed at: $timestamp" &>>$log_file_name
+
 CHECK_ROOT
+
 dnf install mysql-server -y &>>$log_file_name
 VALIDATE $? "Installing mysql"
+
 systemctl enable mysqld &>>$log_file_name
 VALIDATE $? "enabling mysql"
+
 systemctl start mysqld &>>$log_file_name
 VALIDATE $? "starting mysql"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file_name
+
+mysql -h mysql.santhoshdatti.online -u root -pExpenseApp@1 -e 'show databases; &>>$log_file_name
 VALIDATE $? "setting up root password"
